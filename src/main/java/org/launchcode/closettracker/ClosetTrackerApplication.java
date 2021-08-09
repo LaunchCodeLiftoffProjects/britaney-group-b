d46abf7a573288192a894e01f1569a48fa35d244
JediTiger <JediTiger@users.noreply.github.com>
2021-08-09 18:11:01 -0500

    Base starter project code for initial commit
    For the MySQL portion to work you'll need to either make a database and user that matches what's in application.properties or make that file match whatever you make for the project

---
+++ src/main/java/org/launchcode/closettracker/ClosetTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.closettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetTrackerApplication.class, args);
+	}
+
+}
