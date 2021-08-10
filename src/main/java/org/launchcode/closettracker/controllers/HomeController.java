7882bb3c5abd0a0894ae380b6aab015e0ec5b998
Micah <mrusher@gmail.com>
2021-08-10 15:11:56 -0500

    Added controllers, models and templates folders, created index.html and return method for that view that displays "Hello World!"

---
+++ src/main/java/org/launchcode/closettracker/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package org.launchcode.closettracker.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("")
+    public String index (Model model){
+        model.addAttribute("title", "My Closet");
+        return "index";
+    }
+}
