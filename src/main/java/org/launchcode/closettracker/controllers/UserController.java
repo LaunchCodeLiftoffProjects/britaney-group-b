d0ea55a980ccf9f3672550ac21cf885a6861bb4a
JediTiger <JediTiger@users.noreply.github.com>
2021-08-14 14:32:01 -0600

    Making notes on UserController for what it should do

--- src/main/java/org/launchcode/closettracker/controllers/UserController.java
+++ src/main/java/org/launchcode/closettracker/controllers/UserController.java
@@ -1,6 +1,7 @@
 package org.launchcode.closettracker.controllers;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.ui.Model;
 
 public class UserController {
 
@@ -10,10 +11,20 @@ public class UserController {
     and check it and then redirect to the account page or say invalid and retry
 
     What should this controller do?
-    - Capture login credentials from index.html
+    - Capture login credentials from index.html upon submit
+        * Get form field values of username and password
     - Verify if credentials match an existing user
         * Use doesPasswordMatch method in User model to verify if entered password and stored hash match
-    - If so, redirect logged in page to index.html
-    - If not, show an error message and ask to try again
+        - If so, show logged in page
+            * Will need a template for that page but can redirect it to index.html or give it its own location
+        - If not, show an error message and ask to try again
+            * Show errors on same page, no need to redirect
  */
+
+    public String displayAllCategories(Model model) {
+        model.addAttribute("title", "All Categories");
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "eventCategories/index";
+    }
+
 }
