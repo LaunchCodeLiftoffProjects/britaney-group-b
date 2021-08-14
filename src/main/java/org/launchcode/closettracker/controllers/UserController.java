51b68fa16f741388c0311629cf657f8820c7d755
JediTiger <JediTiger@users.noreply.github.com>
2021-08-14 14:27:15 -0600

    Making notes on UserController for what it should do

--- src/main/java/org/launchcode/closettracker/controllers/UserController.java
+++ src/main/java/org/launchcode/closettracker/controllers/UserController.java
@@ -8,5 +8,12 @@ public class UserController {
 /*
     I feel like this controller should be the one to get info from the User view (login page)
     and check it and then redirect to the account page or say invalid and retry
+
+    What should this controller do?
+    - Capture login credentials from index.html
+    - Verify if credentials match an existing user
+        * Use doesPasswordMatch method in User model to verify if entered password and stored hash match
+    - If so, redirect logged in page to index.html
+    - If not, show an error message and ask to try again
  */
 }
