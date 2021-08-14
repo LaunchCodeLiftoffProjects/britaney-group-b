8b3dc4b72fc79887143351b331fbf4f048e6a79d
JediTiger <JediTiger@users.noreply.github.com>
2021-08-13 22:32:04 -0600

    Working to refactor the User model so it only deals with Login/Logout since the CreateController will handle the work of creating a new user.

--- src/main/java/org/launchcode/closettracker/controllers/UserController.java
+++ src/main/java/org/launchcode/closettracker/controllers/UserController.java
@@ -5,5 +5,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class UserController {
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
+/*
+    I feel like this controller should be the one to get info from the User view (login page)
+    and check it and then redirect to the account page or say invalid and retry
+ */
 }
