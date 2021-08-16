4ead4e42a87af6bdc674dc29b4a44acecddd4e18
A M <amymilton99999@gmail.com>
2021-08-16 17:34:42 -0500

    reconfigured for DTO

--- src/main/java/org/launchcode/closettracker/controllers/UserController.java
+++ src/main/java/org/launchcode/closettracker/controllers/UserController.java
@@ -1,6 +1,7 @@
 package org.launchcode.closettracker.controllers;
 
 import org.launchcode.closettracker.models.User;
+import org.launchcode.closettracker.models.dto.UserDTO;
 import org.launchcode.closettracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -8,8 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -20,9 +24,16 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("createUser")
+    @GetMapping("create")
+    public String displayCreateAccountForm(Model model) {
+        model.addAttribute(new UserDTO());
+        model.addAttribute("title", "Create User Account");
+        return "create";
+    }
+
+    @PostMapping("create")
     @ExceptionHandler({SQLException.class, DataAccessException.class})
-    public String createUser(@ModelAttribute @Valid User newUser, Errors errors, Model model) throws IOException {
+    public String createUser(@ModelAttribute @Valid UserDTO userDTO, Errors errors, HttpServletRequest request, Model model) throws IOException {
         try {
             if (errors.hasErrors()) {
                 model.addAttribute("title", "Create User Account");
@@ -30,27 +41,30 @@ public class UserController {
                 return "create";
             }
 
-            if (newUser.getPassword().equals(newUser.getConfirmPassword())
-                    & newUser.isEncodedPasswordEqualsInputPassword(newUser.getPassword())) {
-                userRepository.save(newUser);
-                return "redirect:";
-            } else {
+            User currentUser = userRepository.findByUsername(userDTO.getEmail());
+
+            if (currentUser != null) {
+                errors.rejectValue("username", "username.exists", "An account with this email address already exists");
+                model.addAttribute("title", "Create User Account");
+                return "create";
+            }
+
+            if (!userDTO.getPassword().equals(userDTO.getConfirmPassword())) {
                 model.addAttribute("pwdError", "Passwords do not match");
                 return "create";
             }
-        }
-        catch (Exception ex)
-        {
-            if(ex.toString().contains("constraint"))
-            {
+
+            User newUser = new User(userDTO.getFirstName(), userDTO.getLastName(), userDTO.getEmail(), userDTO.getPassword());
+            userRepository.save(newUser);
+            return "redirect:";
+
+        } catch (Exception ex) {
+            if (ex.toString().contains("constraint")) {
                 model.addAttribute("dbError", "Email exists. Try with new one!");
-            }
-            else
-            {
+            } else {
                 model.addAttribute("dbError", "Db Error");
             }
             return "create";
         }
     }
-
 }
