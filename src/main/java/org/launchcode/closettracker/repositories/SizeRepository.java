89bc36ffdf8ef5df4790e6ab019e501df8cd1355
A M <amymilton99999@gmail.com>
2021-08-26 18:33:37 -0500

    removed size

--- src/main/java/org/launchcode/closettracker/repositories/SizeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.closettracker.repositories;
-
-import org.launchcode.closettracker.models.Size;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SizeRepository extends CrudRepository<Size, Integer>  {
-}
