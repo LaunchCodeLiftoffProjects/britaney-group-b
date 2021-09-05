71dcb3736adfeb3681d1a0d71c4a69d3396c2e45
A M <amymilton99999@gmail.com>
2021-09-04 21:09:08 -0500

    what

---
+++ src/main/java/org/launchcode/closettracker/models/FileUploadUtil.java
@@ -0,0 +1,31 @@
+package org.launchcode.closettracker.models;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+//Directs path and creates directory if it doesn't exist to hold uploaded files
+
+public class FileUploadUtil {
+
+    public static void saveFile(String uploadDirectory, String fileName,
+                                MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get(uploadDirectory);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
+}
