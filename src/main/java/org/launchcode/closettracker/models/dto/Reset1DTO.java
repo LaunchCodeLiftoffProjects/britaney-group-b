5d5862c21430c529b1af394196b525c272fc03b1
JediTiger <JediTiger@users.noreply.github.com>
2021-09-05 21:10:01 -0500

    With reset page working, split page in two. One to handle the reset and temporary process which also send the email and the 2nd the user will use to create a new password.

---
+++ src/main/java/org/launchcode/closettracker/models/dto/Reset1DTO.java
@@ -0,0 +1,48 @@
+package org.launchcode.closettracker.models.dto;
+
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Reset1DTO {
+    // Fields with annotations
+    @NotNull(message = "Temporary password is required")
+    @NotBlank(message = "Temporary password is required")
+    @Transient
+    private String tempPassword;
+
+    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    @Size(min=6, max = 24,  message = "Password must be between 6 and 24 characters long")
+    @Transient
+    private String passwordEntered;
+
+    @NotNull(message = "Confirm password is required")
+    @NotBlank(message = "Confirm password is required")
+    @Size(min=6, max = 24,  message = "Password must be between 6 and 24 characters long")
+    @Transient
+    private String passwordConfirm;
+
+    // Getters & Setters
+    public String getTempPassword() { return tempPassword; }
+
+    public void setTempPassword(String email) { this.tempPassword = tempPassword; }
+
+    public String getPasswordEntered() {
+        return passwordEntered;
+    }
+
+    public void setPasswordEntered(String passwordEntered) {
+        this.passwordEntered = passwordEntered;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+}
--- src/main/java/org/launchcode/closettracker/models/dto/ResetDTO.java
+++ src/main/java/org/launchcode/closettracker/models/dto/ResetDTO.java
@@ -4,7 +4,6 @@ import javax.persistence.Transient;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 public class ResetDTO {
     // Fields with annotations
@@ -14,36 +13,8 @@ public class ResetDTO {
     @Transient
     private String email;
 
-    @NotNull(message = "Password is required")
-    @NotBlank(message = "Password is required")
-    @Size(min=6, max = 24,  message = "Password must be between 6 and 24 characters long")
-    @Transient
-    private String passwordEntered;
-
-    @NotNull(message = "Confirm password is required")
-    @NotBlank(message = "Confirm password is required")
-    @Size(min=6, max = 24,  message = "Password must be between 6 and 24 characters long")
-    @Transient
-    private String passwordConfirm;
-
     // Getters & Setters
     public String getEmail() { return email; }
 
     public void setEmail(String email) { this.email = email; }
 
-    public String getPasswordEntered() {
-        return passwordEntered;
-    }
-
-    public void setPasswordEntered(String passwordEntered) {
-        this.passwordEntered = passwordEntered;
-    }
-
-    public String getPasswordConfirm() {
-        return passwordConfirm;
-    }
-
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
-    }
-}
